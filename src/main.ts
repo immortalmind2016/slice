13dc49e7a31318fa87c5b4210882b67f3b03cbc6
Mohamed Salah <mohamedsalah.software@gmail.com>
2024-01-27 05:57:50 +0200

    chore: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
     session({
-      secret: 'my-secret',
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
       ...(process.env.NODE_ENV == 'production' && {
