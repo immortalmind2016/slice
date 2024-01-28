f4cc79e1a305c075c818194d6108a2b9054f1fe0
Mohamed Salah <mohamedsalah.software@gmail.com>
2024-01-28 03:44:10 +0200

    chore: adding swagger

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as session from 'express-session';
 import 'dotenv/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Slice app')
+    .setDescription('The Slice App API description')
+    .setVersion('1.0')
+    .addTag('slice app')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.use(
     session({
       secret: process.env.SESSION_SECRET,
