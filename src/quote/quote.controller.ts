d81c5ee6db5fa9aa022b155169d5d3bb4b49e02d
Mohamed Salah <mohamedsalah.software@gmail.com>
2024-01-30 01:01:37 +0200

    chore: allow authorId as an input

--- src/quote/quote.controller.ts
+++ src/quote/quote.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { sleep } from '../common/utils';
 import { Auth } from '../decorators/auth.decorator';
 import { QuoteService } from './quote.service';
+import { RequestValidationError } from 'src/common/errors/request-validation.error';
 
 @Controller('quotes')
 export class QuoteController {
   constructor(readonly quoteService: QuoteService) {}
   @Auth()
   @Get('/')
-  async getRandomAuthor() {
+  async getRandomQuote(@Query('authorId') authorId: number) {
+    if (!authorId) {
+      throw new RequestValidationError(['authorId is required']);
+    }
     await sleep(5);
-    return this.quoteService.getRandomAuthor();
+    return this.quoteService.getRandomQuote(authorId);
   }
 }
